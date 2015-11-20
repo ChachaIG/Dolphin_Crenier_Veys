5fd35c73cf6a55fd165e5b2c9f5b1cfbc1c6fd76
ChachaIG <creniercharlotte@gmail.com>
2015-11-20 10:25:12 +0100

    Recherche activity

--- app/src/main/java/com/henallux/dolphin_crenier_veys/view/RechActivity.java
+++ app/src/main/java/com/henallux/dolphin_crenier_veys/view/RechActivity.java
@@ -1,18 +1,79 @@
 package com.henallux.dolphin_crenier_veys.view;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.henallux.dolphin_crenier_veys.R;
 
-public class RechActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class RechActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView recupDateDeb;
+    private TextView recupDateFin;
+    private SimpleDateFormat dateFormatterDeb;
+    private SimpleDateFormat dateFormatterFin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rech);
+        init();
+    }
+
+    public void init(){
+        recupDateDeb = (TextView)findViewById(R.id.recupDateDeb);
+        recupDateDeb.setHint(R.string.indiceDate);
+        recupDateDeb.setOnClickListener(this);
+        recupDateFin = (TextView)findViewById(R.id.recupDateFin);
+        recupDateFin.setHint(R.string.indiceDate);
+        recupDateFin.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                dateFormatterDeb = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+                Calendar newDateDeb = Calendar.getInstance();
+
+                newDateDeb.set(year, monthOfYear, dayOfMonth);
+
+                recupDateDeb.setText(dateFormatterDeb.format(newDateDeb.getTime()));
+
+            }
+        }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+
+        DatePickerDialog dialogFin = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+
+                dateFormatterFin = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+                Calendar newDateFin = Calendar.getInstance();
+
+                newDateFin.set(year, monthOfYear, dayOfMonth);
+             
+                recupDateFin.setText(dateFormatterFin.format(newDateFin.getTime()));
+            }
+        }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+        dialogFin.show();
+
+
     }
 
     @Override
